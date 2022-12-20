756e3fd1b0df35c7a6af4c837a817c8b15dc262c
David Silver <david.silver232@gmail.com>
2022-12-20 01:20:58 -0500

    AndroidView for edit text to receive image URI from the software keyboard.

--- app/src/main/kotlin/io/silv/offlinechat/ui/MessageScreen.kt
+++ app/src/main/kotlin/io/silv/offlinechat/ui/MessageScreen.kt
@@ -1,35 +1,86 @@
 package io.silv.offlinechat.ui
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import android.view.View
+import android.widget.EditText
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material.Button
 import androidx.compose.material.Text
-import androidx.compose.material.TextField
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.view.ContentInfoCompat
+import androidx.core.view.OnReceiveContentListener
+import androidx.core.view.ViewCompat
+import androidx.core.widget.addTextChangedListener
 import io.silv.offlinechat.MainActivityViewModel
+
 
 @Composable
 fun MessageScreen(
     viewModel: MainActivityViewModel
 ) {
-    var text by remember { mutableStateOf("") }
+
+    var text by remember {
+        mutableStateOf("")
+    }
 
     LazyColumn(Modifier.fillMaxSize()) {
         item {
-            TextField(value = text, onValueChange = {
-                text = it
-            })
+            Box(modifier = Modifier.fillMaxWidth().wrapContentHeight()) {
+                ImageEditText(modifier = Modifier.fillMaxSize()) {
+                    text = it
+                }
+            }
+        }
+        item {
             Button(onClick = { viewModel.sendMessageFromClient(text) }) {
-                Text(text = "Send Message")
+                Text(text = "send message")
             }
         }
         items(viewModel.messages) {
-            Text(text = it, modifier = Modifier.padding(8.dp))
+            Text(text = it)
         }
     }
+}
+private class ImageReceiver: OnReceiveContentListener {
+
+    override fun onReceiveContent(view: View, payload: ContentInfoCompat): ContentInfoCompat? {
+        val split = payload.partition { item -> item.uri.also { println("URI $it") } != null}
+        val uriContent = split.first
+        val remaining = split.second
+        uriContent?.let {
+
+        }
+        return remaining
+    }
+
+    companion object {
+         val MIME_TYPES = arrayOf("image/*")
+    }
+}
+
+@Composable
+fun ImageEditText(
+    modifier: Modifier,
+    onTextChange: (String) -> Unit
+) {
+
+    AndroidView(
+        modifier = modifier,
+        factory = { context ->
+            // Creates view
+            EditText(context).apply {
+                // Sets up listeners for View -> Compose communication
+                ViewCompat.setOnReceiveContentListener(
+                    this, ImageReceiver.MIME_TYPES, ImageReceiver()
+                )
+                addTextChangedListener {
+                    onTextChange(it.toString())
+                }
+            }
+        },
+        update = { }
+    )
 }
